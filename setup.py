4dc0b1eaac0086bc5eb79f0f71d399df3330c6c3
Wouter van Atteveldt <wouter@vanatteveldt.com>
2019-02-25 18:14:02 +0100

    Add long_description

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from distutils.core import setup
 
 setup(
     name="embeddingvectorizer",
-    version="0.01",
+    version="0.02",
     description="Sklearn vectorizers using word embedding model",
+    long_description=("Package to create a scikit-learn vectorizer that uses a word2vec model."
+                      "Has options for mean, max, and sum of vectors. Not properly tested yet, use at your own risk!"),
     author="Wouter van Atteveldt",
     author_email="wouter@vanatteveldt.com",
     packages=["embeddingvectorizer"],
